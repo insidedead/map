3ba35c66936405c1ab97b8e6506e3d9cbcfe97a1
insidedead <nhocosousa@outlook.com>
2022-06-27 18:04:19 +0200

    map

--- map.cpp
+++ map.cpp
@@ -1,7 +1,10 @@
 #include <cstddef>
 #include <memory>
 #include <algorithm>
+
+#ifdef DB
 #include <iostream>
+#endif
 
 #define ALLOC(vec, size) \
 	vec->first = vec->track = vec->alloc.allocate(size); \
@@ -16,11 +19,13 @@ struct Pair {
 	U second;
 };
 
+#ifdef DB
 template <class T, class U>
 Pair<T, U>::~Pair<T, U>()
 {
-	
+	std::cout << "destructor called" << std::endl;
 }
+#endif
 
 template <class T, class U>
 class Map {
@@ -31,11 +36,15 @@ class Map {
 	iterator last;
 	iterator track;
 
+	protected:
+		void grow(std::size_t size);
+
 	public:
 		Map(): first(0), last(0) {};
 		Map( iterator, iterator );
 		Map( const Map& );
 		explicit Map(std::size_t s) { ALLOC(this, s); };
+		~Map();
 
 		iterator begin() const { return first; };
 		iterator end() const { return last; };
@@ -46,6 +55,7 @@ class Map {
 		void insert( Pair<T, U>& );
 		void erase( T );
 		std::ptrdiff_t erase( iterator, iterator );
+		void swap( Map& );
 };
 
 template <class T, class U>
@@ -62,6 +72,20 @@ Map<T, U>::Map( const Map &m )
 	{
 		ALLOC(this, m.size());
 		track = std::uninitialized_copy(m.begin(), m.end(), first);
+	}
+}
+
+template <class T, class U>
+Map<T, U>::~Map()
+{
+	if(first)
+	{
+		while(last-- != first)
+		{
+			alloc.destroy(last);
+		}
+
+		alloc.deallocate(first, last - first);
 	}
 }
 
@@ -85,30 +109,79 @@ void Map<T,U>::insert( Pair<T, U> &p )
 }
 
 template <class T, class U>
-void Map<T, U>::erase(T key)
+void Map<T, U>::erase( T key )
 {
 	Pair<T, U>* it = std::find_if(first, last, [=] ( Pair<T, U> p ) { return p.first == key; });
 
 	if(it)
 	{
 		alloc.destroy(it);
+		delete it;
+		
 		--track;
 	}
 }
 
+template <class T, class U>
+std::ptrdiff_t Map<T, U>::erase( Pair<T, U> *begin, Pair<T, U> *end )
+{
+	while(end-- != begin)
+	{
+		alloc.destroy(end);
+		delete end;
+	}
+
+	track = begin;
+
+	return end - begin;
+}
+
+template <class T, class U>
+void Map<T, U>::swap( Map<T, U> &m )
+{
+	std::pair<Pair<T,U>*, int> temp = std::get_temporary_buffer<Pair<T, U>>(this->size());
+	
+	std::uninitialized_copy(this->first, this->last, temp.first);
+
+	if(this != &m)
+	{
+		if(m.size() > size()) grow(m.size());
+		if(size() > m.size()) m.grow(size());
+
+		std::uninitialized_copy(m.begin(), m.end(), first);
+		std::uninitialized_copy(temp.first, temp.first + temp.second, m.first);
+	}
+}
+
+template <class T, class U>
+void Map<T, U>::grow(std::size_t size)
+{
+	delete this;
+	ALLOC(this, size);
+}
+
 int main()
 {
+#ifdef DB
 	Pair<char, int> t;
 	t.first = 'a';
 	t.second = 69;
 
+	Pair<char, int> oo;
+	oo.first = 'v';
+	oo.second = 11;
+
 	Map<char, int> p(5);
-
+	Map<char, int> pp(5);
+	
 	p.insert(t);
+	pp.insert(oo);
 
 	std::cout << p['a'];
 
-	p.erase('a');
+	p.swap(pp);
 
-	std::cout << p['a'];
+	std::cout << p['v'];
+#endif
+	return 0;
 }
